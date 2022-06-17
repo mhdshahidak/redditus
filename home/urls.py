b797c290b6f4d7428c05eb74084e51dfd70c3038
mhdshahidak <mhdshd.ak@gmail.com>
2022-06-17 15:07:34 +0530

    new changes

---
+++ home/urls.py
@@ -0,0 +1,18 @@
+from . import views
+from django.urls import path
+
+app_name ='home'
+
+urlpatterns = [
+
+    path('', views.index, name="index"),
+    path('billing', views.bill, name="billing"),
+    path('client',views.client,name='client'),
+    path('addclient',views.addclient,name='addclient'),
+    path('Return',views.Return,name='Return'),
+    path('index',views.index,name='index'),
+    path('stock', views.stock, name="stock"),
+    path('stock_edit', views.stock_edit, name="stock_edit"),
+
+  
+]
---
+++ home/views.py
@@ -0,0 +1,23 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+def index(request):
+    return render(request,'home.html')
+
+def bill(request):
+    return render(request,'invoice.html')
+def client(request):
+    return render(request,'client.html')
+
+def addclient(request):
+    return render(request,'addclient.html')
+
+def Return(request):
+    return render(request,'return.html')
+
+def stock(request):
+    return render(request, 'stock.html')
+
+def stock_edit(request):
+    return render(request, 'edit-stock.html')
